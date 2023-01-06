bc096494914f818ce4dc38633922ff9114957b49
Andy <kwikius@yahoo.com>
2023-01-06 19:17:36 +0000

    ws only

--- src/core/BuiltinContext.h
+++ src/core/BuiltinContext.h
@@ -7,13 +7,16 @@
 #include "Context.h"
 
 class EvaluationSession;
+class InstantiableModule;
 
 class BuiltinContext : public Context
 {
 public:
   void init() override;
-  boost::optional<CallableFunction> lookup_local_function(const std::string& name, const Location& loc) const override;
-  boost::optional<InstantiableModule> lookup_local_module(const std::string& name, const Location& loc) const override;
+  boost::optional<CallableFunction>
+    lookup_local_function(const std::string& name, const Location& loc) const override;
+  boost::optional<InstantiableModule>
+    lookup_local_module(const std::string& name, const Location& loc) const override;
 
 protected:
   BuiltinContext(EvaluationSession *session);
