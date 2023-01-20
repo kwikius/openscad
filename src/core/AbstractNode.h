533dc706dbe38f0d5c298692f5c60bd9d28c7a3d
Andy <kwikius@yahoo.com>
2023-01-20 23:45:46 +0000

    Make some function nodiscard in AbstractNode.h

--- src/core/AbstractNode.h
+++ src/core/AbstractNode.h
@@ -14,7 +14,8 @@ extern int progress_report_count;
 extern void (*progress_report_f)(const std::shared_ptr<const AbstractNode>&, void *, int);
 extern void *progress_report_vp;
 
-void progress_report_prep(const std::shared_ptr<AbstractNode>& root, void (*f)(const std::shared_ptr<const AbstractNode>& node, void *vp, int mark), void *vp);
+void progress_report_prep(const std::shared_ptr<AbstractNode>& root,
+   void (*f)(const std::shared_ptr<const AbstractNode>& node, void *vp, int mark), void *vp);
 void progress_report_fin();
 
 /*!
@@ -22,15 +23,14 @@ void progress_report_fin();
    tree.  Both the module tree and the node tree are regenerated from
    scratch for each compile.
  */
-class AbstractNode : public Visitable<void,AbstractNode>, public std::enable_shared_from_this<AbstractNode>
-{
+class AbstractNode : public Visitable<void,AbstractNode>,
+   public std::enable_shared_from_this<AbstractNode>{
   // FIXME: the idx_counter/idx is mostly (only?) for debugging.
   // We can hash on pointer value or smth. else.
   //  -> remove and
   // use smth. else to display node identifier in CSG tree output?
   static size_t idx_counter; // Node instantiation index
 public:
-//  VISITABLE();
   AbstractNode(const ModuleInstantiation *mi);
   virtual std::string toString() const;
   /*! The 'OpenSCAD name' of this node, defaults to classname, but can be
@@ -54,21 +54,18 @@ public:
     return this->children;
   }
 
-  Location const & getLocation() const { return getModInst().location();}
-  bool isHighlight() const { return getModInst().isHighlight();}
-  bool isRoot() const {return getModInst().isRoot();}
-  bool isBackground() const { return getModInst().isBackground();}
+  [[nodiscard]] Location const & getLocation() const { return getModInst().location();}
+  [[nodiscard]] bool isHighlight() const { return getModInst().isHighlight();}
+  [[nodiscard]] bool isRoot() const {return getModInst().isRoot();}
+  [[nodiscard]] bool isBackground() const { return getModInst().isBackground();}
 
-
-
-  bool hasSameModInst(std::shared_ptr<AbstractNode> const & other) const
+  [[nodiscard]] bool hasSameModInst(std::shared_ptr<AbstractNode> const & other) const
   { return &this->getModInst() == &other->getModInst();}
 
-  size_t index() const { return this->idx; }
+  [[nodiscard]] size_t index() const { return this->idx; }
 
   static void resetIndexCounter() { idx_counter = 1; }
 private:
-  // FIXME: Make protected
   std::vector<std::shared_ptr<AbstractNode>> children;
   const ModuleInstantiation *modinst;
   int idx; // Node index (unique per tree)
