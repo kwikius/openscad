184546f4f3a17cef27e45e9189344303e821af49
Andy <kwikius@yahoo.com>
2023-01-20 23:43:04 +0000

    streamline State.h

--- src/core/State.h
+++ src/core/State.h
@@ -11,14 +11,13 @@
   * &brief struct for returning various AbstractNode state
   * usually from a Visitor
 **/
-class State{
+class State final{
 public:
   State(const std::shared_ptr<const AbstractNode> &parent)
-    : flags(NONE), parentnode(parent), numchildren(0) {
-    this->matrix_ = Transform3d::Identity();
-    this->color_.fill(-1.0f);
-  }
-  virtual ~State() {}
+    : flags(NONE), parentnode(parent), numchildren(0),
+     matrix_{ Transform3d::Identity()},
+     color_{-1.0f,-1.0f,-1.0f,-1.0f}
+    { }
 
 private:
     enum StateFlags {
@@ -32,28 +31,28 @@ private:
    template <StateFlags Flag>
    void setFlag( bool v) { this->flags = (v)?(this->flags | Flag ):(this->flags & ~Flag);}
    template <StateFlags Flag>
-   bool getFlag() const { return (this->flags & Flag) ? true : false;}
+   [[nodiscard]] bool getFlag() const { return (this->flags & Flag) ? true : false;}
 public:
   void setPrefix(bool v) { setFlag<PREFIX>(v); }
-  bool isPrefix() const { return getFlag<PREFIX>(); }
+  [[nodiscard]] bool isPrefix() const { return getFlag<PREFIX>(); }
   void setPostfix(bool v) { setFlag<POSTFIX>(v); }
-  bool isPostfix() const { return getFlag<POSTFIX>(); }
+  [[nodiscard]] bool isPostfix() const { return getFlag<POSTFIX>(); }
   void setHighlight(bool v) { setFlag<HIGHLIGHT>(v); }
-  bool isHighlight() const { return getFlag<HIGHLIGHT>(); }
+  [[nodiscard]] bool isHighlight() const { return getFlag<HIGHLIGHT>(); }
   void setBackground(bool v) { setFlag<BACKGROUND>(v); }
-  bool isBackground() const { return getFlag<BACKGROUND>(); }
+  [[nodiscard]] bool isBackground() const { return getFlag<BACKGROUND>(); }
   void setPreferNef(bool v) { setFlag<PREFERNEF>(v); }
-  bool preferNef() const { return getFlag<PREFERNEF>(); }
+  [[nodiscard]] bool preferNef() const { return getFlag<PREFERNEF>(); }
 
   void setNumChildren(unsigned int numc) { this->numchildren = numc; }
   void setParent(const std::shared_ptr<const AbstractNode> &parent) { this->parentnode = parent; }
   void setMatrix(const Transform3d& m) { this->matrix_ = m; }
   void setColor(const Color4f& c) { this->color_ = c; }
 
-  unsigned int numChildren() const { return this->numchildren; }
-  std::shared_ptr<const AbstractNode> parent() const { return this->parentnode; }
-  const Transform3d& matrix() const { return this->matrix_; }
-  const Color4f& color() const { return this->color_; }
+  [[nodiscard]] unsigned int numChildren() const { return this->numchildren; }
+  [[nodiscard]] std::shared_ptr<const AbstractNode> parent() const { return this->parentnode; }
+  [[nodiscard]] const Transform3d& matrix() const { return this->matrix_; }
+  [[nodiscard]] const Color4f& color() const { return this->color_; }
 
 private:
 
